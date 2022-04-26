d87853ed4c457f7db133d54684fee7b0216eef43
Heejun <ysyang706@gmail.com>
2022-04-26 18:02:16 +0900

    update

--- scrapper/13_selenium.py
+++ scrapper/13_selenium.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-browser = webdriver.Chrome("./chromedriver")
+from webdriver_manager.chrome import ChromeDriverManager
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://developers.google.com/maps/documentation/geocoding/overview")
 elem = browser.find_element_by_id("query-input")
 elem.send_keys
